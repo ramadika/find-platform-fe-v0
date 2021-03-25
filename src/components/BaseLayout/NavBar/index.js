d0e04bfd3b0de516e3caa2186f555cb1d1272f55
ramadika <rzdka18@gmail.com>
2021-03-25 15:32:17 +0700

    navbar

--- src/components/BaseLayout/NavBar/index.js
+++ src/components/BaseLayout/NavBar/index.js
@@ -8,7 +8,7 @@ import 'components/BaseLayout/NavBar/index.css'
 export default function index() {
     return (
         <div>
-            <Navbar bg="light" variant="light" expand="lg">
+            <Navbar bg="dark" variant="dark" expand="lg">
                 <NavLink className="navbar-brand" to="/">Cipta<span>Things</span>.</NavLink>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse className="justify-content-center" id="basic-navbar-nav">
