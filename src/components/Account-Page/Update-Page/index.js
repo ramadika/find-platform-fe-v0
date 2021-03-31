a704034786d1d35026d8647a40d6c872c9060823
ramadika <rzdka18@gmail.com>
2021-03-31 11:30:48 +0700

    update

--- src/components/Account-Page/Update-Page/index.js
+++ src/components/Account-Page/Update-Page/index.js
@@ -15,7 +15,7 @@ export default class index extends Component {
                         <form onSubmit={this.postData}>
                             <h3 className="text-center"><b>Device ID <span style={{color:'red'}}>*</span></b></h3>
                             <h4 className="text-center">Scan Device to get the ID</h4>
-                            <input type="text" name="address" ref={(val) => this.address = val} className="text-center form-control" style={{width:'220px',marginLeft:'auto',marginRight:'auto'}}></input>
+                            <input type="text" name="address" ref={(val) => this.address = val} className="text-center form-control" style={{width:'220px',marginLeft:'auto',marginRight:'auto'}} disabled></input>
                             <div className="row justify-content-center mt-3">
                                 <div className="col">
                                     <h3><b>Display Name <span style={{color:'red'}}>*</span></b></h3>
@@ -28,18 +28,18 @@ export default class index extends Component {
                                     <input type="text" name="fullName" ref={(val) => this.fullName = val} className="form-control" required></input>
                                 </div>
                             </div>
-                            <div className="row">
+                            <div className="row mt-4 delete-update">
                                 <div className="d-flex bd-highlight">
-                                    <div className="p-2 w-100 bd-highlight">
-                                        <h3>Delete ID</h3>
+                                    <div className="p-2 w-100 bd-highlight mr-5">
+                                        <h3><b>Delete ID</b></h3>
                                         <h4>By deleting your device ID you have to scan the device again to get the ID</h4>
                                     </div>
-                                    <div className="p-2 flex-shrink-1 bd-highlight">
-                                        <h4><b>Delete account..</b></h4>
+                                    <div className="p-2 flex-shrink-0 bd-highlight d-flex align-items-center ml-5">
+                                        <h4 style={{fontSize:'8pt', fontWeight:'600'}}>Delete account..</h4>
                                     </div>
                                 </div>
                             </div>
-                            <input type="submit" value="Submit"  className="form-control mt-5"></input>
+                            <input type="submit" value="Submit"  className="form-control mt-4"></input>
                         </form>
                     </div>
                 </div>
