c23550e9eb4a3cfcdebd6f4669ee4c1785972e73
ramadika <rzdka18@gmail.com>
2021-04-12 10:49:03 +0700

    navbar

--- src/components/BaseLayout/NavBar/index.js
+++ src/components/BaseLayout/NavBar/index.js
@@ -11,15 +11,15 @@ export default function index() {
             <Navbar bg="dark" variant="dark" expand="lg">
                 <NavLink className="d-flex justify-content-start align-self-center navbar-brand" to="/home">Cipta<span>Things</span>.</NavLink>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                <Navbar.Collapse className="d-flex justify-content-center align-self-center" id="basic-navbar-nav">
+                <Navbar.Collapse className="justify-content-center" id="basic-navbar-nav">
                     <Nav>
                         <NavLink to="/dashboard">Dashboard</NavLink>
                         <NavLink to="/reports">Reports</NavLink>
                         <NavLink to="/help">Help</NavLink>
                         <NavLink to="/account">Account</NavLink>
                     </Nav>
+                    <NavLink className="ml-auto navbar-out" to="/">Sign out</NavLink>
                 </Navbar.Collapse>
-                <NavLink className="d-flex justify-content-end align-self-center navbar-out" to="/">Sign out</NavLink>
             </Navbar>
         </div>
     )
