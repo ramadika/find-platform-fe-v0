3b8e875bae4fc72074b6144864867eb253280c68
ramadika <rzdka18@gmail.com>
2021-04-14 11:12:54 +0700

    csupport

--- src/components/Help-Page/CSupport-Page/index.js
+++ src/components/Help-Page/CSupport-Page/index.js
@@ -2,10 +2,10 @@
 import React, { Component } from 'react'
 import Axios from 'axios';
 import swal from "sweetalert";
+import { AiFillInstagram, AiOutlineGlobal, AiFillFacebook } from 'react-icons/ai'
 // Internals
 import 'components/Help-Page/CSupport-Page/index.css'
 import img1 from 'assets/Cipta_Things-removebg.png'
-import img2 from 'assets/mario-gogh-VBLHICVh-lI-unsplash.jpg'
 import { DataContext } from 'components/Context'
 
 export default class index extends Component {
@@ -66,16 +66,19 @@ export default class index extends Component {
             <div className="csupport">
                 <div className="container">
                     <div className="row">
-                        <div className="col">
-                            <img src={img2} alt="img2" style={{width:'575px', height:'auto'}}></img>
+                        <div className="col d-flex align-items-end section1-csupport">
                             <div className="row">
                                 <div className="col">
+                                    <h2><b>Find us</b></h2>
                                     <h4>
                                         <span><b>North Cikarang</b></span>
                                         <span>Jababeka XIV</span>
                                         <span>hello@mail.com</span>
                                         <span><a href="tel:082218358955">+02 1234 567</a></span>
                                     </h4>
+                                    <a target="_blank" rel="noopener noreferrer" href="https://www.google.com/" className="ml-5"><AiOutlineGlobal size={25}/></a>
+                                    <a target="_blank" rel="noopener noreferrer" href="https://www.google.com/" className="ml-4"><AiFillInstagram size={25}/></a>
+                                    <a target="_blank" rel="noopener noreferrer" href="https://www.google.com/" className="ml-4"><AiFillFacebook size={25}/></a>
                                 </div>
                                 {/* <div className="col">
                                     <h4>
@@ -95,17 +98,17 @@ export default class index extends Component {
                                 <div className="row justify-content-center mt-3">
                                     <div className="col">
                                         <h3><b>First Name <span style={{color:'red'}}>*</span></b></h3>
-                                        <input type="text" name="firstName" ref={(val) => this.firstName = val} className="form-control" required></input>
+                                        <input type="text" name="firstName" ref={(val) => this.firstName = val} className="form-control" placeholder="First Name" required></input>
                                     </div>
                                     <div className="col">
                                         <h3><b>Last Name <span style={{color:'red'}}>*</span></b></h3>
-                                        <input type="text" name="lastName" ref={(val) => this.lastName = val} className="form-control" required></input>
+                                        <input type="text" name="lastName" ref={(val) => this.lastName = val} className="form-control" placeholder="Last Name" required></input>
                                     </div>
                                 </div>
                                 <h3><b>Email Address <span style={{color:'red'}}>*</span></b></h3>
-                                <input type="email" name="email" ref={(val) => this.email = val} className="form-control" pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$" required></input>
+                                <input type="email" name="email" ref={(val) => this.email = val} className="form-control" pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$" placeholder="Email Address" required></input>
                                 <h3><b>Message <span style={{color:'red'}}>*</span></b></h3>
-                                <textarea name="message" ref={(val) => this.message = val} className="form-control" rows="4" cols="50" required></textarea>
+                                <textarea name="message" ref={(val) => this.message = val} className="form-control" rows="4" cols="50" placeholder="Message" required></textarea>
                                 <input type="submit" value="Get in touch"  className="form-control my-4"></input>
                             </form>
                         </div>
